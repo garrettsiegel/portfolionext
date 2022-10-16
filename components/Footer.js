0fedd58565d82f54db9897ba28ab34e6d89b13c1
Garrett Siegel <garrett@submurgedcreative.com>
2022-10-16 09:48:39 -0400

    edits

--- components/Footer.js
+++ components/Footer.js
@@ -16,12 +16,12 @@ const Footer = () => {
                     </div>
                     </a></Link>
 
-                    <Link href="https://calendly.com/garrettsiegel/phonecall" ><a target={"_blank"}>
+                    {/* <Link href="https://calendly.com/garrettsiegel/phonecall" ><a target={"_blank"}>
                     <div className={styles.footerleftflex}>
                     <Image src="/calendar-icon.svg" width={35} height={35} layout={"intrinsic"} alt={''}/>
                     <span>Book A Calendly Meeting</span>
                     </div>
-                    </a></Link>
+                    </a></Link> */}
                 </div>
                 <div className={styles.footerright}>
                     <Link href="https://github.com/garrettsiegel/"><a target={"_blank"}>
